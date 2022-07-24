cfa8b0cd0d278f05b922dfec703c08c38f812c8d
Leston <lestonair6574@gmail.com>
2022-07-24 13:14:10 +0530

    css problem solved

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import "./todo.css"
+import React from "react";
+import "./todo.css";
 
 function Todo() {
   return (
-    <div className='one'>
+    <div className="one">
       <div className="firstimg">
-        <img src="https://cdn.pixabay.com/photo/2016/12/21/05/44/reminder-1922255_960_720.png" alt="Sorry error" className="oneimg" />
-      
+        <img
+          src="https://cdn.pixabay.com/photo/2016/12/21/05/44/reminder-1922255_960_720.png"
+          alt="Sorry error"
+          className="oneimg"
+        />
       </div>
-    <div className="para">
-    <p>Add your list here ✌👇</p>
-    </div>
+      <div className="para">
+        <p>Add your list here ✌👇</p>
+      </div>
       <div className="addItems">
-        <input type="text" placeholder='Enter your note ✍✍'  />
+        <div className="additemsone">
+          <input type="text" placeholder="Enter your note✍✍" />
+        </div>
+        <div className="additemstwo">
+          <div className="leston">
+            <i class="fa-solid fa-circle-plus fa-2x"></i>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
